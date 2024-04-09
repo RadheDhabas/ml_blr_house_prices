4137949e8f44e8d437fc6ee2ef90a6f9a6bac51e
RadheDhabas <101125809+RadheDhabas@users.noreply.github.com>
2024-04-09 17:10:48 +0530

    Update util.py

--- util.py
+++ util.py
@@ -2,38 +2,32 @@ import pickle
 import json
 import numpy as np
 
-__model = None
-__data_columns = None
-__locations = None
-
-def get_estimated_price(location,sqft,bhk,bath,model,data_columns):
+def get_estimated_price(location,sqft,bhk,bath):
     try:
-        loc_index = data_columns.index(location.lower())
+        location_index = data_columns.index(location.lower())
     except:
-        loc_index = -1
-
+        location_index = -1
+    model,data_columns = load_saved_artifacts()[0],load_saved_artifacts()[2]
     x = np.zeros(len(data_columns))
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index>=0:
-        x[loc_index] = 1
+    if location_index>=0:
+        x[location_index] = 1
     return round(model.predict([x])[0],2)
 
 def get_locations():
-    return __locations
+    locations = load_saved_artifacts()[1]
+    return locations
 
 def load_saved_artifacts():
     print("loading saved artifacts...start")
-    global  __data_columns
-    global __locations
-
+    
     with open("./model files/columns.json", "r") as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]  # first 3 columns are sqft, bath, bhk
-    global __model
-    if __model is None:
-        with open('./model files/bangalore_home_price_model.pickle', 'rb') as f:
-            __model = pickle.load(f)
+        data_columns = json.load(f)['data_columns']
+        locations = data_columns[3:]  # first 3 columns are sqft, bath, bhk
+    
+    with open('./model files/bangalore_home_price_model.pickle', 'rb') as f:
+        model = pickle.load(f)
     print("loading saved artifacts...done")
-    return __model,__locations,__data_columns
+    return [model,locations,data_columns]
